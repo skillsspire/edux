a1125e5052e8b6a9a11dc8893f550ee6b2d862d9
Sofya <106378879+lolalilmao@users.noreply.github.com>
2025-08-23 22:50:20 +0500

    Fix missing signup view

--- app/urls.py
+++ app/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     # Страницы аутентификации (Django предоставляет их из коробки, нам нужны только шаблоны)
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('signup/', views.signup, name='signup'), # Это представление нам нужно будет создать позже
+    #path('signup/', views.signup, name='signup'), # Это представление нам нужно будет создать позже
     path('password_reset/', auth_views.PasswordResetView.as_view(template_name='registration/password_reset.html'), name='password_reset'),
     # Каталог и детали курса
     path('courses/', views.courses_list, name='courses_list'),
