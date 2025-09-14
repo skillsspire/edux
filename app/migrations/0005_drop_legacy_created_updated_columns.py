4077939db11195bc7968ac7c538f1059852c44b7
Sofya <skillsspire@gmail.com>
2025-09-14 23:16:30 +0500

    migration(app): drop legacy DB-only columns created/updated from app_course

--- app/migrations/0005_drop_legacy_created_updated_columns.py
+++ app/migrations/0005_drop_legacy_created_updated_columns.py
@@ -9,14 +9,9 @@ class Migration(migrations.Migration):
     operations = [
         migrations.RunSQL(
             sql="""
-                ALTER TABLE app_course
-                DROP COLUMN IF EXISTS created,
-                DROP COLUMN IF EXISTS updated;
+                ALTER TABLE app_course DROP COLUMN IF EXISTS created;
+                ALTER TABLE app_course DROP COLUMN IF EXISTS updated;
             """,
-            reverse_sql="""
-                ALTER TABLE app_course
-                ADD COLUMN IF NOT EXISTS created timestamp with time zone NULL,
-                ADD COLUMN IF NOT EXISTS updated timestamp with time zone NULL;
-            """,
+            reverse_sql=migrations.RunSQL.noop,
         ),
     ]
