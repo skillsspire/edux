0e1cf18312bd9e7286bfc968cfc5e5e774e09144
Sofya <skillsspire@gmail.com>
2025-09-22 22:16:32 +0500

    fix(migrations): пропустить postgres DO $$ на SQLite; feat(models): slug blank=True, UniqueConstraint, валидаторы

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/yelenagordeyeva/Downloads/edux-main/.venv"
+set -gx VIRTUAL_ENV "/Users/yelenagordeyeva/SkillsSpire/edux/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
