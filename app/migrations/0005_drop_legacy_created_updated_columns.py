ee6c477ab23c6ee5142b7a25a2225ef3a567c36a
Sofya <skillsspire@gmail.com>
2025-09-24 01:16:54 +0500

    Сноска

--- app/migrations/0005_drop_legacy_created_updated_columns.py
+++
@@ -1,8 +0,0 @@
-from django.db import migrations
-
-class Migration(migrations.Migration):
-    dependencies = [
-        ("app", "0004_course_is_popular_lessonprogress"),
-    ]
-    # На SQLite ничего не делаем; на Postgres эту очистку мы когда-нибудь проведём отдельно.
-    operations = []
--- app/migrations/0006_force_drop_legacy_created_updated.py
+++
@@ -1,7 +0,0 @@
-from django.db import migrations
-
-class Migration(migrations.Migration):
-    dependencies = [
-        ("app", "0005_drop_legacy_created_updated_columns"),
-    ]
-    operations = []
