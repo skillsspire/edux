137d6d4e304ece0ef26d512e08f4063dfd796081
Sofya <skillsspire@gmail.com>
2025-12-05 23:48:06 +0500

    Fix captcha import error by adding captcha URLs

--- app/urls.py
+++ app/urls.py
@@ -58,6 +58,8 @@ urlpatterns = [
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     path("signup/", views.signup, name="signup"),
 
+    path("captcha/", include("captcha.urls")),
+
     # --- Сброс пароля ---
     path("password-reset/", auth_views.PasswordResetView.as_view(
         template_name="registration/password_reset_form.html"
